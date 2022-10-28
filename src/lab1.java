4d8560acac4a741a0cf9c33a6147ab35a69378ce
unknown <lesha3320@gmail.com>
2022-10-28 16:49:46 +0300

    uploaded lab1

--- src/lab1.java
+++ src/lab1.java
@@ -1,5 +1,39 @@
+// zalikova 0521
+// C2 = 1
+// C3 = 2
+// C5 = 1
+// C7 = 3
+// C = 2
+// O1 -
+// O2 /
+// i and j long
+
+
 public class lab1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    long a = 4;
+    long b = 1;
+    int c = 2;
+    int n = 9;
+    int m = 11;
+
+
+    if (a > n || b > m) {
+        System.out.println("Equation error");
+        return;
+    }
+
+    double result = 0;
+
+    for (long i = a; i <= n; i++) {
+        for (long j = b; j <= m; j++) {
+            if ((i - c) == 0) {
+                throw new IllegalStateException("Cant divide by 0");}
+            result += ((i / j) / (i - c));
+        }
+
+    }
+
+        System.out.println(result);
     }
 }
